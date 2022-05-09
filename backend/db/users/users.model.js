3662b68222d79d2e076a63c6661f60fa1e9fb0a3
Thomas Ryan <DWTrip@gmail.com>
2022-05-09 13:55:36 +0900

    Feat: Build basic structure for users model. Includes getAll, getById, create, and update models

---
+++ backend/db/users/users.model.js
@@ -0,0 +1,43 @@
+const knex = require("../knex");
+
+const usersTable = "users";
+
+module.exports = {
+  getAll(limit = 100) {
+    return knex
+      .select({
+        id: "id",
+        userId: "userId",
+        startDate: "startDate",
+      })
+      .from(usersTable)
+      .limit(limit);
+  },
+
+  getById(id) {
+    return knex
+      .select({
+        id: "id",
+        userId: "userId",
+        startDate: "startDate",
+      })
+      .from(usersTable)
+      .where({ id: id })
+      .first();
+  },
+
+  create(user) {
+    // validation
+
+    return knex.insert(user).into("users").catch(console.error());
+  },
+
+  update(id, userUpdate) {
+    return knex("users")
+      .update(userUpdate)
+      .where("id", id)
+      .returning("id")
+      .then((id) => id[0].id)
+      .catch(console.error());
+  },
+};
